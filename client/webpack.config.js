49fa4d38c9f536650fdbbf51afcda218cbbb9ddc
lianajayde <lianajaydee@gmail.com>
2023-04-24 13:41:21 -0500

    added work plug ins and loaders

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,52 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+        title: "Webpack Plug",
+      }),
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
+      }),
+
+      new WebpackPwaManifest({
+        name: "A Text Editor",
+        short_name: "JATE",
+        description: "A text editor for your own use",
+        background_color: "#7eb4e2",
+        theme_color: "#7eb4e2",
+        fingerprints: false,
+        inject: true,
+        start_url: "/",
+        publicPath: "/",
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons"),
+          },
+        ],
+      }),
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins:["@babel/plugin-proposal-object-rest-spread","@babel/transform-runtime"],
+            },
+          },
+        },
       ],
     },
   };
